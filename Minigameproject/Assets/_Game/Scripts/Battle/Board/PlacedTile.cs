79f9aef5f94b860c928d0165cd62323885127669
김선진 <rlatjswls14@gmail.com>
2026-05-19 16:54:31 +0900

    에너미와  폴더링

---
+++ Minigameproject/Assets/_Game/Scripts/Battle/Board/PlacedTile.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlacedTile : MonoBehaviour, IPointerClickHandler
+{
+    private CardData cardData;
+    private BoardManager boardManager;
+    private HandManager handManager;
+    private int placedId = -1;
+    private int originIndex = -1;
+
+    public bool IsActivePlacement => cardData != null;
+
+    public void Setup(CardData data, BoardManager board, HandManager hand, int id, int origin)
+    {
+        cardData = data;
+        boardManager = board;
+        handManager = hand;
+        placedId = id;
+        originIndex = origin;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!IsActivePlacement) return;
+
+        boardManager.RemoveCard(originIndex);
+        handManager.AddCard(cardData);
+        Destroy(gameObject);
+    }
+}
