6fece1aca9db1cab2ac5e6bcba471024a7fc1a5c
김선진 <rlatjswls14@gmail.com>
2026-05-21 14:41:20 +0900

    feat: 캐릭터/에너미 애니메이션 (Idle/Hit/Attack) + World Space Canvas

---
+++ Minigameproject/Assets/_Game/Scripts/Battle/Player/PlayerAnimator.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Vector3 originalPosition;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        originalPosition = transform.localPosition;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(IdleAnimation());
+    }
+
+    // Idle: 살짝 위아래로 둥실둥실
+    private IEnumerator IdleAnimation()
+    {
+        float time = 0f;
+        while (true)
+        {
+            time += Time.deltaTime;
+            float offsetY = Mathf.Sin(time * 1.5f) * 0.05f;
+            transform.localPosition = originalPosition + new Vector3(0, offsetY, 0);
+            yield return null;
+        }
+    }
+
+    // 피격: 빨간 깜빡임 + 흔들림
+    public IEnumerator HitAnimation()
+    {
+        StopCoroutine(IdleAnimation());
+
+        for (int i = 0; i < 3; i++)
+        {
+            spriteRenderer.color = new Color(1f, 0.3f, 0.3f, 1f);
+            transform.localPosition = originalPosition + new Vector3(-0.1f, 0, 0);
+            yield return new WaitForSeconds(0.05f);
+
+            spriteRenderer.color = Color.white;
+            transform.localPosition = originalPosition + new Vector3(0.1f, 0, 0);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        spriteRenderer.color = Color.white;
+        transform.localPosition = originalPosition;
+
+        StartCoroutine(IdleAnimation());
+    }
+
+    public void PlayHit()
+    {
+        StartCoroutine(HitAnimation());
+    }
+}
