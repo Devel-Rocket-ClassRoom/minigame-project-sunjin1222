e08373cc2c3a291e8d74fa82e6aae48a5b2077bf
김선진 <rlatjswls14@gmail.com>
2026-05-21 14:41:06 +0900

    feat: 랜덤 적 스폰 시스템 (BattleManager + 프리팹 인스턴스화)

--- Minigameproject/Assets/_Game/Scripts/Battle/Enemy/EnemyUI.cs
+++ Minigameproject/Assets/_Game/Scripts/Battle/Enemy/EnemyUI.cs
@@ -6,20 +6,36 @@ public class EnemyUI : MonoBehaviour
 {
     public Slider slider;
     public TextMeshProUGUI healthText;
-
-
     public TextMeshProUGUI intentText;
     public Image intentIcon;
+
+    private Canvas worldCanvas;
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        worldCanvas = GetComponentInChildren<Canvas>();
+        mainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        // World Space Canvas가 항상 카메라를 향하도록
+        if (worldCanvas != null && mainCamera != null)
+        {
+            worldCanvas.transform.LookAt(
+                worldCanvas.transform.position + mainCamera.transform.rotation * Vector3.forward,
+                mainCamera.transform.rotation * Vector3.up
+            );
+        }
+    }
 
     public void UpdateUI(int currentHealth, int maxHealth)
     {
         slider.maxValue = maxHealth;
         slider.value = currentHealth;
-
-        healthText.text =
-            $"{currentHealth} / {maxHealth}";
+        healthText.text = $"{currentHealth} / {maxHealth}";
     }
-
 
     public void UpdateIntent(EnemyPattern pattern)
     {
