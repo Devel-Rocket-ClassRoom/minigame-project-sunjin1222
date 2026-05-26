1076e984b7a8b8f0b5911019720ec355da683a95
김선진 <rlatjswls14@gmail.com>
2026-05-26 19:39:57 +0900

    feat: 카드 모양 표시와 보드 재배치 추가

--- Minigameproject/Assets/_Game/Scripts/Battle/Card/CardView.cs
+++ Minigameproject/Assets/_Game/Scripts/Battle/Card/CardView.cs
@@ -15,6 +15,10 @@ public class CardView : MonoBehaviour, IPointerClickHandler
 
     public CardData GetCardData() => cardData;
 
+    public RectTransform shapePreviewRoot;
+    public Image miniTileTemplate;
+    public float miniTileSize = 7f;
+
     public void Setup(CardData data, Action<CardData> onClick = null)
     {
         cardData = data;
@@ -37,11 +41,60 @@ public class CardView : MonoBehaviour, IPointerClickHandler
                 cardImage.sprite = null;
                 cardImage.enabled = false;
             }
+            BuildShapePreview(data);
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         onClickCallback?.Invoke(cardData);
+    }
+    private void BuildShapePreview(CardData data)
+    {
+        if (shapePreviewRoot == null || miniTileTemplate == null || data == null)
+            return;
+
+        for (int i = shapePreviewRoot.childCount - 1; i >= 0; i--)
+        {
+            Transform child = shapePreviewRoot.GetChild(i);
+
+            if (child.gameObject != miniTileTemplate.gameObject)
+                Destroy(child.gameObject);
+        }
+
+        if (data.tileShape == null || data.tileShape.Length == 0)
+            return;
+
+        Vector2Int min = data.tileShape[0];
+        Vector2Int max = data.tileShape[0];
+
+        for (int i = 1; i < data.tileShape.Length; i++)
+        {
+            min = Vector2Int.Min(min, data.tileShape[i]);
+            max = Vector2Int.Max(max, data.tileShape[i]);
+        }
+
+        float shapeWidth = (max.x - min.x + 1) * miniTileSize;
+        float shapeHeight = (max.y - min.y + 1) * miniTileSize;
+        float startX = (shapePreviewRoot.rect.width - shapeWidth) * 0.5f;
+        float startY = (shapePreviewRoot.rect.height - shapeHeight) * 0.5f;
+
+        for (int i = 0; i < data.tileShape.Length; i++)
+        {
+            Vector2Int offset = data.tileShape[i] - min;
+
+            Image tile = Instantiate(miniTileTemplate, shapePreviewRoot);
+            tile.gameObject.SetActive(true);
+
+            RectTransform tileRect = tile.rectTransform;
+            tileRect.anchorMin = new Vector2(0f, 1f);
+            tileRect.anchorMax = new Vector2(0f, 1f);
+            tileRect.pivot = new Vector2(0f, 1f);
+            tileRect.sizeDelta = new Vector2(miniTileSize, miniTileSize);
+            tileRect.anchoredPosition = new Vector2(
+                startX + offset.x * miniTileSize,
+                -startY - offset.y * miniTileSize
+            );
+        }
     }
 }
